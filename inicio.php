662906956de946947e16bf2cce1dac1ab072cbb2
Santiago Bonilla <santiagobonillaeguren@gmail.com>
2021-05-31 20:25:15 -0300

    Primer Commit

---
+++ inicio.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Inicio</title>
+</head>
+<body>
+    <form id="formulario" action="formulario.php" method="post">
+        Nombre: <input type="text" name="nombre" id="nombre"><br>
+        Apeliido: <input type="text" name="apellido" id="apellido"><br>
+        Telefono: <input type="text" name="telefono" id="telefono"><br>
+        Edad: <input type="text" name="edad" id="edad"><br>
+        FechadeNacimiento: <input type="text" name="fechadeNacimiento" id="fechaNacimiento"><br>
+        Email: <input type="text" name="email" id="email"><br>
+        <input type="sumbit" value="Enviar">
+    </form>
+</body>
