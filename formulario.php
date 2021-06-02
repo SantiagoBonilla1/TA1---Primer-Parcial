68e613d85ea24b2e0c4b32bf991cc6aa102c0097
Santiago Bonilla <santiagobonillaeguren@gmail.com>
2021-06-01 22:28:09 -0300

    Agregar confirmar campos vacíos y edad mayor a 0

--- formulario.php
+++ formulario.php
@@ -6,14 +6,22 @@
 	$fechadeNacimiento = $_POST['fechadeNacimiento'];
 	$email = $_POST['email'];
 	$conexion = mysqli_connect("localhost", "root", "", "escritophp");
+	guardar($nombre, $apellido, $telefono, $edad, $fechadeNacimiento, $email, $conexion)
 	
-	public function guardar(){
-	$sql = "INSERT INTO persona(nombre,apellido,telefono,edad,fechadeNacimiento,email) VALUES (
-            '$nombre',
-            '$apellido',
-            '$edad',
-            '$email'}
+	function guardar($nombreG, $apellidoG, $telefonoG, $edadG, $fechadeNacimientoG, $emailG, $conexionG){
+		if($nombre !== "" && $apelllido !== "" && $telefono !== "" && $edad !== "" && $fechadeNacimiento !== "" && $email !== "" && edad > 0){
+			$sql = "INSERT INTO persona(nombre,apellido,telefono,edad,fechadeNacimiento,email) VALUES (
+            '$nombreG',
+            '$apellidoG',
+            '$telefonoG',
+            '$edadG',
+            '$fechadeNacimientoG',
+            '$emailG'}
 			)";
+			echo("Persona Agregada correctamente")
+		}else{
+			header("HTTP/1.1 500 Internal server error");
+			echo("Hubo un problema al procesar la solicitud")
+		}
 	mysqli_query($conexion, $sql);
 	}
-	
