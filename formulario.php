6e5108bb6a85803dada5fa9682936ff70b7cceef
Santiago Bonilla <santiagobonillaeguren@gmail.com>
2021-06-01 22:44:57 -0300

    Agregado Clean Code, falta agregarlo a la función guardar;

--- formulario.php
+++ formulario.php
@@ -5,11 +5,11 @@
 	$edad = $_POST['edad'];
 	$fechadeNacimiento = $_POST['fechadeNacimiento'];
 	$email = $_POST['email'];
-	$conexion = mysqli_connect("localhost", "root", "", "escritophp");
+	$conexion = new mysqli("127.0.0.1","root","","escritophp");
 	guardar($nombre, $apellido, $telefono, $edad, $fechadeNacimiento, $email, $conexion)
 	
 	function guardar($nombreG, $apellidoG, $telefonoG, $edadG, $fechadeNacimientoG, $emailG, $conexionG){
-		if($nombre !== "" && $apelllido !== "" && $telefono !== "" && $edad !== "" && $fechadeNacimiento !== "" && $email !== "" && edad > 0){
+		if(validardatosingresados){
 			$sql = "INSERT INTO persona(nombre,apellido,telefono,edad,fechadeNacimiento,email) VALUES (
             '$nombreG',
             '$apellidoG',
@@ -23,5 +23,26 @@
 			header("HTTP/1.1 500 Internal server error");
 			echo("Hubo un problema al procesar la solicitud")
 		}
-	mysqli_query($conexion, $sql);
+		mysqli_query($conexion, $sql);
 	}
+	
+	function validardatosingresados($nombre, $apellido, $telefono, $edad, $fechadeNacimiento, $email){
+        if(!confirmarCamposVacios($nombre, $apellido, $telefono, $edad, $fechadeNacimiento, $email)) return false;
+        if(!confirmarNumerico($edad)) return false;
+		
+        return true;
+    }
+	
+	function confirmarCamposVacios($nombreCCV, $apellidoCCV, $telefonoCCV, $edadCCV, $fechadeNacimientoCCV, $emailCCV){
+        if($nombreCCV === "" || $apellidoCCV === "" || $telefonoCCV === "" || $edadCCV === "" || $fechadeNacimientoCCV === "" || $emailCCV === ""){
+            return false;
+        }
+        return true;
+    }
+
+    function confirmarNumerico($numero){
+        if(!is_numeric($numero)){
+            return false;
+        }
+        return true;
+    }
